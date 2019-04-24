8adf2b6ef36a1e3e73ff2f4b18e5b0a0346700ed
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-24 15:09:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,10 @@
 # code here!
 class School
-  
+  attr_reader :roster
   
   def initialize(name)
     @name = name
     @roster = {}
-  end
-  def roster
-    @roster
   end
   def add_student(student, grade)
     if @roster[grade] != nil 
